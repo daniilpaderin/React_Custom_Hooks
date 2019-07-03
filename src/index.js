e0463b93ae39dd6ba674abcc94d903f381cf7775
Daniil Paderin <49814572+daniilpaderin@users.noreply.github.com>
2019-07-03 17:36:04 +0300

    work version 1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
 
+function useDebounce(value, delay) {
+  const [currentValue, setCurrentValue] = useState(null);
+
+  useEffect(() => {
+    let timerID = setTimeout(() => setCurrentValue(value), delay);
+    return () => {
+      clearTimeout(timerID);
+    };
+  });
+
+  return currentValue;
+}
+
 function App() {
+  const delay = 2000;
+  const [query, setQuery] = useState("");
+  const debouncedQuery = useDebounce(query, delay);
+
+  const handleInputChange = event => setQuery(event.target.value);
+
   return (
     <div className="App">
-      <h1>Hello React Cudfasdfstom Hooks</h1>
+      <input type="text" value={query} onChange={handleInputChange} />
+      <p>Query: {query}</p>
+      <p>Debounced Query: {debouncedQuery}</p>
     </div>
   );
 }
